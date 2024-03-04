c38dd468081721e0e6f239c32b29136713f2e713
Aditya Agrawal <2021ugee043@nitjsr.ac.in>
2024-03-04 22:01:28 +0530

    added search functionality

---
+++ src/components/SearchPage.tsx
@@ -0,0 +1,105 @@
+import { useSearchRestaurants } from "@/api/SearchRestaurantApi";
+import { useParams } from "react-router-dom";
+import SearchResultInfo from "./SearchResultInfo";
+import SearchResultsCard from "./SearchResultsCard";
+import { useEffect, useState } from "react";
+import SearchBar, { SearchForm } from "./SearchBar";
+import PaginationSelector from "./PaginationSelector";
+import CuisineFilter from "./CuisineFilter";
+import SortOptionDropdown from "./SortOptionDropdown";
+
+export type SearchState = {
+  searchQuery: string;
+  page: number;
+  selectedCuisines: string[];
+  sortOption: string;
+};
+const SearchPage = () => {
+  const { city } = useParams();
+  const [searchState, setSearchState] = useState<SearchState>({
+    searchQuery: "",
+    page: 1,
+    selectedCuisines: [],
+    sortOption: "bestMatch",
+  });
+  const [isExpanded, setIsExpanded] = useState<boolean>(false);
+
+  const { results, isLoading } = useSearchRestaurants(searchState, city);
+
+  const setSortOption = (sortOption: string) => {
+    setSearchState((prevState) => ({
+      ...prevState,
+      sortOption,
+      page: 1,
+    }));
+  };
+
+  const setSelectedCuisines = (selectedCuisines: string[]) => {
+    setSearchState((prevState) => ({
+      ...prevState,
+      selectedCuisines,
+    }));
+  };
+  const setPage = (page: number) => {
+    setSearchState((prevState) => ({
+      ...prevState,
+      page,
+    }));
+  };
+  const setSearchQuery = (searchFormdata: SearchForm) => {
+    setSearchState((prevState) => ({
+      ...prevState,
+      searchQuery: searchFormdata.searchQuery,
+      page: 1,
+    }));
+  };
+  const resetSearch = () => {
+    setSearchState((prevState) => ({
+      ...prevState,
+      searchQuery: "",
+    }));
+  };
+  if (isLoading) {
+    return <span>Loading...</span>;
+  }
+  if (!results?.data || !city) {
+    return <span>No results Found!</span>;
+  }
+
+  return (
+    <div className="grid grid-cols-1  lg:grid-cols-[250px_1fr] gap-5">
+      <div id="cuisines-list">
+        <CuisineFilter
+          selectedCuisines={searchState.selectedCuisines}
+          onChange={setSelectedCuisines}
+          isExpanded={isExpanded}
+          onExpandedClick={() => setIsExpanded((prev) => !prev)}
+        />
+      </div>
+      <div id="main-content" className="flex flex-col gap-5">
+        <SearchBar
+          searchQuery={searchState.searchQuery}
+          onSubmit={setSearchQuery}
+          placeholder="Search By Cuisine or restaurant name"
+          onReset={resetSearch}
+        />
+        <div className=" flex justify-between flex-col gap-3 lg:flex-row">
+          <SearchResultInfo city={city} total={results.pagination.total} />
+          <SortOptionDropdown
+            sortOption={searchState.sortOption}
+            onChange={(value) => setSortOption(value)}
+          />
+        </div>
+        {results.data.map((restaurant) => (
+          <SearchResultsCard restaurant={restaurant} />
+        ))}
+        <PaginationSelector
+          page={results.pagination.page}
+          pages={results.pagination.pages}
+          onPageChange={setPage}
+        />
+      </div>
+    </div>
+  );
+};
+export default SearchPage;
---
+++ src/components/SearchResultsCard.tsx
@@ -0,0 +1,50 @@
+import { Restaurant } from "@/types";
+import { Link } from "react-router-dom";
+import { AspectRatio } from "./ui/aspect-ratio";
+import { Clock, Dot, Wallet2 } from "lucide-react";
+
+type Props = {
+  restaurant: Restaurant;
+};
+
+const SearchResultsCard = ({ restaurant }: Props) => {
+  return (
+    <Link
+      to={`details/${restaurant._id}`}
+      className="grid lg:grid-cols-[2fr_3fr] gap-5 group"
+    >
+      <AspectRatio ratio={16 / 6}>
+        <img
+          src={restaurant.imageUrl}
+          className="rounded-md w-full h-full object-cover"
+        ></img>
+      </AspectRatio>
+      <div className="">
+        <h3 className="text-2xl mb-2 tracking-tight group-hover:underline font-bold">
+          {restaurant.restaurantName}
+        </h3>
+        <div id="card-content" className="grid md:grid-cols-2 gap-2">
+          <div className="flex flex-row flex-wrap">
+            {restaurant.cuisines.map((item, index) => (
+              <span className="flex">
+                <span>{item}</span>
+                {index < restaurant.cuisines.length - 1 && <Dot />}
+              </span>
+            ))}
+          </div>
+          <div className="flex gap-2 flex-col">
+            <div className="flex items-center gap-1 text-green-600">
+              <Clock className="text-green-600" />
+              {restaurant.estimatedDeliveryTime} mins
+            </div>
+            <div className="flex items-center gap-1">
+              <Wallet2 />
+              Delivery from ₹{(restaurant.deliveryPrice / 100).toFixed(2)}
+            </div>
+          </div>
+        </div>
+      </div>
+    </Link>
+  );
+};
+export default SearchResultsCard;
